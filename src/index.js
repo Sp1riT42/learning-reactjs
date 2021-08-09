4605c8a880bb047f9ea4cee574c252b4873404ed
Sp1riT42 <pohunov@gmail.com>
2021-08-09 18:37:13 +0300

    Add hw3

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {createTheme, ThemeProvider} from "@material-ui/core/styles";
+import {orange} from "@material-ui/core/colors";
+const theme = createTheme({
+    customTheme: {
+        color: orange[500]
+    },
+    palette: {
+        secondary: {
+            main: orange[500],
+        },
+        primary: {
+            main: orange[500]
+        }
+    },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
